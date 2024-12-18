9bc55e1a8ad91bb7ea1b56d11c8b57637526dfa7
huang-tc <tzu.chun.huang.tw@gmail.com>
2024-12-18 12:18:10 +0800

    refactor: Move discount calculation method to Order class

--- src/feature_envy/customer.py
+++ src/feature_envy/customer.py
@@ -6,10 +6,4 @@ class Customer:
         self.order = Order(order_amount)
     
     def calculate_discount(self) -> float:
-        return self._get_discount_amount()
-    
-    def _get_discount_amount(self) -> float:
-        if self.order.amount > 500:
-            return self.order.amount * 0.1
-        else:
-            return 0
+        return self.order.get_discount_amount()
--- src/feature_envy/order.py
+++ src/feature_envy/order.py
@@ -1,3 +1,9 @@
 class Order:
     def __init__(self, amount: float):
         self.amount = amount
+
+    def get_discount_amount(self) -> float:
+        if self.amount > 500:
+            return self.amount * 0.1
+        else:
+            return 0
