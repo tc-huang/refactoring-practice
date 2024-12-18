5ab422d60ab235254cef929b0a8cf7baa21739cf
huang-tc <tzu.chun.huang.tw@gmail.com>
2024-12-18 15:57:52 +0800

    refactor: Extract parameter object DateRange from Booking class

--- src/data_clump_primitive_obsession/booking.py
+++ src/data_clump_primitive_obsession/booking.py
@@ -1,10 +1,11 @@
+from data_clump_primitive_obsession.date_range import DateRange
+
 class Booking:
     def __init__(self, guest_name: str, start_date: str, end_date: str, price_per_night: float):
         self.guest_name = guest_name
-        self.start_date = start_date
-        self.end_date = end_date
+        self.date_range = DateRange(start_date, end_date)
         self.price_per_night = price_per_night
     
     def calculate_total_price(self) -> float:
-        days = (int(self.end_date.split('-')[2]) - int(self.start_date.split('-')[2]))
+        days = self.date_range.get_number_of_days()
         return days * self.price_per_night
