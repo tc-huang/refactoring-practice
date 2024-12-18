88881ffbe23014aeb0fed88d1b0e5393824b2020
huang-tc <tzu.chun.huang.tw@gmail.com>
2024-12-18 16:50:17 +0800

    feat: Implement Student and StudentHelper classes with full name retrieval functionality

--- src/data_class/helper.py
+++ src/data_class/helper.py
@@ -1,2 +1,6 @@
+from data_class.student import Student
+
 class StudentHelper:
-    pass
+    @staticmethod
+    def get_full_name(student: Student) -> str:
+        return student.first_name + " " + student.last_name
