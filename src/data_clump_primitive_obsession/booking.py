a0b2c74f7c61dfda2202d0210fc4768745f1d528
huang-tc <tzu.chun.huang.tw@gmail.com>
2024-12-18 15:47:06 +0800

    feat: Implement Booking class with total price calculation method

--- src/data_clump_primitive_obsession/booking.py
+++ src/data_clump_primitive_obsession/booking.py
@@ -1,2 +1,10 @@
 class Booking:
-    pass
+    def __init__(self, guest_name: str, start_date: str, end_date: str, price_per_night: float):
+        self.guest_name = guest_name
+        self.start_date = start_date
+        self.end_date = end_date
+        self.price_per_night = price_per_night
+    
+    def calculate_total_price(self) -> float:
+        days = (int(self.end_date.split('-')[2]) - int(self.start_date.split('-')[2]))
+        return days * self.price_per_night
